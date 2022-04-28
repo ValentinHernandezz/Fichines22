a69f17be4d272012b1d8e4d6cb77ac878fe3cc7d
LeoChaparro <chaparro.leonardo.et.12@gmail.com>
2022-04-28 12:11:02 -0300

    das
    das

--- BD/00-ddl.sql
+++ BD/00-ddl.sql
@@ -2,37 +2,13 @@ DROP DATABASE IF EXISTS fichines;
 create database fichines;
 USE fichines;
 create table Fichin (
- idFichin TINYINT(200) NOT NULL,
+idFichin TINYINT(200) NOT NULL,
 Nombre VARCHAR(25) NOT NULL,
 Lanzamiento Year NOT NULL, 
 Precio DECIMAL(7,2) NOT NULL,
-idRecarga  TINYINT(200) NOT NULL, 
-PRIMARY KEY (idFicihin),
-FOREIGN KEY (idRecarga)
-REFERENCES ficihines.Recarga (idRecarga)
+PRIMARY KEY (idFichin)
 );
-create table Jugada (
-idJugada TINYINT(200) NOT NULL,
-idFichin BIGINT NOT NULL,
-idRecarga TINYINT(200) NOT NULL,
-Fechahora DATETIME NOT NULL,
-CreditoGastados DECIMAL(7,2),
-PRIMARY KEY (idJugada),
-CONSTRAINT fk_jugada_Fichin FOREIGN KEY (idFichin)
-REFERENCES fichines (idFichin)
-CONSTRAINT fk_jugada_Recarga FOREIGN KEY (idRecarga)
-REFERENCES Recarga (idRecarga)
-);
-
-create table Recarga (
-idRecarga TINYINT(200) NOT NULL,
-DNI INT NOT NULL,
-FechaHora DATETIME NOT NULL,
-Monto DECIMAL(7.2) NOT NULL,
-PRIMARY KEY (idRecarga),
-CONSTRAINT fk_DNI_Recarga FOREIGN KEY (DNI)
-REFERENCES Recarga (DNI)
-);
+    
 
 create table Cliente (
 DNI INT NOT NULL, 
@@ -43,3 +19,24 @@ TarjetaJuego SMALLINT NOT NULL,
 PRIMARY KEY (DNI)
 );
 
+create table Recarga (
+idRecarga TINYINT(200) NOT NULL,
+DNI INT NOT NULL,
+FechaHora DATETIME NOT NULL,
+Monto DECIMAL(7.2) NOT NULL,
+PRIMARY KEY (idRecarga),
+FOREIGN KEY (DNI)
+);
+
+create table Jugada (
+idJugada TINYINT(200) NOT NULL,
+idFichin BIGINT NOT NULL,
+idRecarga TINYINT(200) NOT NULL,
+Fechahora DATETIME NOT NULL,
+CreditoGastados DECIMAL(7,2),
+PRIMARY KEY (idJugada),
+CONSTRAINT fk_jugada_Fichin FOREIGN KEY (idFichin)
+REFERENCES Jugada.fichin (idFichin)
+CONSTRAINT fk_Cliente_Recarga FOREIGN KEY (idRecarga)
+REFERENCES jugada.Recarga (idRecarga)
+);
