a1bdffcd529f48b63d2736d19f44700512c08c19
LeoNardochaparro1 <chaparro.leonardo.et12@gmail.com>
2022-06-10 08:54:01 -0300

    SP altas a todas las entidades, Sp RegisterCliente
    Valen Fijate en cambiar El Der, le tenes que agregar la tabla Pass
    Pass CHAR(64)

--- BD/00-ddl.sql
+++ BD/00-ddl.sql
@@ -18,6 +18,8 @@ DNI INT NOT NULL,
 Nombre VARCHAR(45) NOT NULL,
 Apellido VARCHAR(45) NOT NULL,
 Mail VARCHAR(64) NOT NULL,
+Saldo DECIMAL(8,2) NOT NULL,
+Pass (char 64) NOT NULL,
 TarjetaJuego SMALLINT UNSIGNED NOT NULL,
 PRIMARY KEY (DNI)
 );
@@ -44,3 +46,28 @@ REFERENCES Fichin (idFichin),
 CONSTRAINT fk_Cliente_Recarga FOREIGN KEY (idRecarga)
 REFERENCES Recarga (idRecarga)
 );
+delimiter $$
+drop procedure if exists altaFichin $$
+procedure altaFichin (unidFichin TINYINT, unNombre VARCHAR(45), unLanzamiento YEAR, unPrecio, unidRecarga TINYINT)
+BEGIN
+insert into Fichin (idFichin, Nombre, Lanzamiento, Precio, idRecarga)
+             value (unidFichin, unNombre, unLanzamiento, unPrecio, unidRecarga);
+              end $$
+drop procedure if exists altaJugada $$
+create procedure altaJugada (unidJugada TINYINT, unidFichin TINYINT, unidRecarga TINYINT, unFechaHora DATETIME, unCreditoGastado DECIMAL(7,2))
+BEGIN
+insert into Jugada (idJugada, idFichin, idRecarga, FechaHora, CreditosGastados)
+             value (unidJugada, unidFichin, unidRecarga, unFechaHora, unCreditosGastados);
+               end $$
+drop procedure if exists altaRecarga $$
+create procedure altaRecarga (unidRecarga TINYINT, unDNI INT, unFechaHora DATETIME, unMonto DECIMAL(7,2))
+BEGIN
+insert into Recarga (idRecarga, DNI, FechaHora, Monto)
+              value (unidRecarga,unDNI, unFechahora, unMonto);
+drop procedure if exists registrarCliente $$
+procedure registrarCliente (unDNI INT, unNombre VARCHAR(45), unApellido VARCHAR(45), unMail VARCHAR(45), unTarjetaJuego SMALLINT UNSIGNED, unSaldo DECIMAL(8,2) unPass(CHAR 64))
+BEGIN
+insert into (DNI, Nombre, Apellido, Mail, TarjetaJuego, Saldo, Pass) 
+      value (unDNI, unNombre, un Apellido, unMail, unTarjetoJuego, unSaldo, sha2(unPass 256));
+      end $$
+
