0c8d984786f07151c57ff5949c385de9bb3ccb3e
ValentinHernandezz <valentin.hernandez.et12@gmail.com>
2022-07-07 10:31:37 -0300

    guardado

--- BD/01-SP.sql
+++ BD/01-SP.sql
@@ -9,8 +9,8 @@ end $$
 drop procedure if exists altaJugada $$
 create procedure altaJugada (unidJugada TINYINT, unidFichin TINYINT, unidRecarga TINYINT, unFechaHora DATETIME, unCreditoGastado DECIMAL(7,2))
 BEGIN
-insert into Jugada (idJugada, idFichin, idRecarga, FechaHora, CreditosGastados)
-             values (unidJugada, unidFichin, unidRecarga, unFechaHora, unCreditosGastados);
+insert into Jugada (idJugada, idFichin, idRecarga, FechaHora, CreditoGastado)
+             values (unidJugada, unidFichin, unidRecarga, unFechaHora, unCreditoGastado);
 end $$
 
 drop procedure if exists altaRecarga $$
@@ -21,7 +21,9 @@ insert into Recarga (idRecarga, DNI, FechaHora, Monto)
 end $$
 
 drop procedure if exists registrarCliente $$
-create procedure registrarCliente (unDNI INT, unNombre VARCHAR(45), unApellido VARCHAR(45), unMail VARCHAR(45), unTarjetaJuego SMALLINT UNSIGNED, unSaldo DECIMAL(8,2) unPass CHAR(64))
+create procedure registrarCliente (unDNI INT, unNombre VARCHAR(45), unApellido VARCHAR(45), 
+                                  unMail VARCHAR(45), unTarjetaJuego SMALLINT UNSIGNED, 
+                                  unSaldo DECIMAL(8,2) unPass CHAR(64))
 BEGIN
 insert into Cliente (DNI, Nombre, Apellido, Mail, TarjetaJuego, Saldo, Pass) 
       values (unDNI, unNombre, unApellido, unMail, unTarjetaJuego, unSaldo, SHA2(unPass, 256));
@@ -34,28 +36,39 @@ DELIMITER $$
 DROP PROCEDURE IF EXISTS clientePorDniPass $$
 CREATE PROCEDURE  clientePorDniPass  (unDNI INT,unPass CHAR(64))
 BEGIN 
-    if EXISTS (SELECT * FROM Cliente WHERE unDNI = DNI AND unPass = Pass )
+
         SELECT *
         FROM Cliente;
-    end if;
+        WHERE unDNI = DNI AND sha2(unPass, 256) = Pass;
 END $$    
 
 
 
 
-/*Se pide hacer el SF ‘RecaudacionPara’ que reciba por parámetro un identificador de fichin, 
-se debe devolver la ganancia que tuvo entre esas 2 fechas (inclusive).*/
+--Se pide hacer el SF ‘RecaudacionPara’ que reciba por parámetro un identificador de fichin, 
+--se debe devolver la ganancia que tuvo entre esas 2 fechas (inclusive).
 DELIMITER $$
 DROP FUNCTION IF EXISTS RecaudacionPara $$
-CREATE FUNCTION RecaudacionPara (unid_Fichin TINYINT,)
-                                                                     RETURNS INT READS SQL DATA
+CREATE FUNCTION RecaudacionPara (unid_Fichin TINYINT,FechaHora DATETIME)
+                                 RETURNS INT READS SQL DATA
 BEGIN
   DECLARE resultado FLOAT;
- SELECT SUM( Credito_Gastado) INTO resultado
+SELECT SUM(Credito_Gastado) INTO resultado
   FROM Jugada
   WHERE id_Jugada = unid_Jugada
-  AND Fecha_Hora BETWEEN cotaInferior AND cotaSuperior; 
-   
+  AND FechaHora BETWEEN FechaHora; 
+
 RETURN resultado;
 
 END $$
+--Se pide hacer el SP ‘Gastos’ que reciba por parámetro una identificación de cliente. El SP tiene que devolver fecha y hora, nombre del juego y consumo ($) que el cliente haya jugado, ordenado por fecha y hora descendentemente.
+DELIMITER $$
+DROP PROCEDURE IF EXISTS Gastos
+create procedure Gastos (DNI INT,fechaHora DATE TIME,nombre VARCHAR(45),creditosGastado DECIMAL (7,2))
+BEGIN
+select *
+from jugada 
+join Fichin using (idFichin)
+where fechaHora desc;
+end $$
+
